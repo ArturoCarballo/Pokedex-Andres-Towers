4db88e59809cb8be147caeb6f4a71d691166e76c
ArturoCarballo <a01662245@tec.mx>
2024-02-28 20:37:06 -0600

    Ya arregle sus pinches demadres
    - Monkeyboy y Mecoboy

--- src/components/NintendoDS.tsx
+++ src/components/NintendoDS.tsx
@@ -17,16 +17,23 @@ const NintendoDS: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center">
-      <div className="top-screen">
-        {/* Contenido de la pantalla superior, pokedex */}
+    <div className="flex flex-col items-center justify-center h-screen my-5" style={{ maxHeight: 'calc(100vh - 3rem)' }}>
+      {/* Pantalla Superior */}
+      <div className="parteSuperior bg-gray-900 w-[50%] h-[50%] border-solid border-black border-8 rounded-t-[70px]">
+        <div className='pantallaSuperior bg-white w-[70%] h-[80%] border-solid border-black border-8 rounded-lg m-auto'></div>
       </div>
-      <div className="controller">
-        {/* Botones direccionales LB corner */}
+
+      {/* Sección Central */}
+      <div className="bg-gray-800 w-[51%] h-[05%] border-solid border-black border-8 relative rounded-full">
+
+      </div>
+
+      {/* Pantalla Inferior */}
+      <div className="flex bg-sky-700 w-[50%] h-[50%] border-solid border-black border-8 rounded-b-[70px] shadow-2xl">
         <div className="directional-pad">
-        <ArrowButton direction="" onClick={() => console.log('Up button clicked')} /> 
-          <ArrowButton direction="up" onClick={() => console.log('Up button clicked')} /> 
-          <ArrowButton direction="" onClick={() => console.log('Up button clicked')} /> 
+          <ArrowButton direction="" onClick={() => console.log('Up button clicked')} />
+          <ArrowButton direction="up" onClick={() => console.log('Up button clicked')} />
+          <ArrowButton direction="" onClick={() => console.log('Up button clicked')} />
           <ArrowButton direction="left" onClick={() => console.log('Left button clicked')} />
           <ArrowButton direction="" onClick={() => console.log('Left button clicked')} />
           <ArrowButton direction="right" onClick={() => console.log('Right button clicked')} />
@@ -34,25 +41,15 @@ const NintendoDS: React.FC = () => {
           <ArrowButton direction="down" onClick={() => console.log('Down button clicked')} />
           <ArrowButton direction="" onClick={() => console.log('Down button clicked')} />
         </div>
+        <div className='pantallaInferior bg-white w-[60%] h-[90%] border-solid border-black border-8 rounded-b-lg m-auto'>
+          <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}></SearchBar>
+          <Keyboard handleClick={handleKeyPress} ></Keyboard>
+        </div>
       </div>
-      <div className="bottom-screen">
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}></SearchBar>
-        <Keyboard handleClick={handleKeyPress} ></Keyboard>
-      </div>
-
-  {/* Sección Central */}
-  <div className="bg-gray-800 w-[51%] h-[05%] border-solid border-black border-8 relative rounded-full">
-
-  </div>
-
-  {/* Pantalla Inferior */}
-  <div className="bg-sky-700 w-[50%] h-[50%] border-solid border-black border-8 rounded-b-[70px] shadow-2xl">
-    <div className='pantallaInferior bg-white w-[60%] h-[90%] border-solid border-black border-8 rounded-b-lg m-auto'></div>
-  </div> 
-</div>
+    </div>
 
 
-  
+
   );
 }
 
