30fe1ce6360b493f91881d7d9f2c8a319cd39289
MoyBadillo117 <A00834306@tec.mx>
2024-02-28 20:08:02 -0600

    Diseño
    Diseño de nintendo

--- src/components/NintendoDS.tsx
+++ src/components/NintendoDS.tsx
@@ -4,6 +4,7 @@ import ArrowButton from './arrowbutton';
 import SearchBar from './SearchBar/SearchBar';
 import Keyboard from './Keyboard/Keyboard';
 
+
 const NintendoDS: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -16,27 +17,25 @@ const NintendoDS: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center">
-      <div className="top-screen">
-        {/* Contenido de la pantalla superior, pokedex */}
-      </div>
-      <div className="controller">
-        {/* Botones direccionales LB corner */}
-        <div className="directional-pad">
-          <ArrowButton direction="up" onClick={() => console.log('Up button clicked')} />
-          <div>
-            <ArrowButton direction="left" onClick={() => console.log('Left button clicked')} />
-            <ArrowButton direction="right" onClick={() => console.log('Right button clicked')} />
-          </div>
-          <ArrowButton direction="down" onClick={() => console.log('Down button clicked')} />
-        </div>
-      </div>
-      <div className="bottom-screen">
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}></SearchBar>
-        <Keyboard handleClick={handleKeyPress} ></Keyboard>
-      </div>
+    <div className="flex flex-col items-center justify-center h-screen my-5" style={{ maxHeight: 'calc(100vh - 3rem)' }}>
+  {/* Pantalla Superior */}
+  <div className="parteSuperior bg-gray-900 w-[50%] h-[50%] border-solid border-black border-8 rounded-t-[70px]">
+    <div className='pantallaSuperior bg-white w-[70%] h-[80%] border-solid border-black border-8 rounded-lg m-auto'></div>
+  </div>
 
-    </div>
+  {/* Sección Central */}
+  <div className="bg-gray-800 w-[51%] h-[05%] border-solid border-black border-8 relative rounded-full">
+
+  </div>
+
+  {/* Pantalla Inferior */}
+  <div className="bg-sky-700 w-[50%] h-[50%] border-solid border-black border-8 rounded-b-[70px] shadow-2xl">
+    <div className='pantallaInferior bg-white w-[60%] h-[90%] border-solid border-black border-8 rounded-b-lg m-auto'></div>
+  </div> 
+</div>
+
+
+  
   );
 }
 
