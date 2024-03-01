fb7c7feff4c9be8c30d45719386781c0fb5c67e9
Alfredo-Vqz <a01656384@tec.mx>
2024-02-29 18:12:41 -0600

    tab bar mejor

---
+++ src/components/PkmDisplay/PkmDisplay.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LeftPanel = () => {
+    return (
+        <div className="left-panel-content flex flex-col justify-start items-center h-full">
+            <div className="title-box bg-gray-900 rounded-lg p-2 mt-1">
+                <h2 className="text-center text-white">Pokemon</h2>
+            </div>
+            {/* Content for the left panel (e.g., Pokemon image) */}
+        </div>
+    );
+}
+
+export default LeftPanel;
