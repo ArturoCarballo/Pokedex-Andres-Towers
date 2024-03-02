1c14dc0035df637864df3e4ea71b703d8bce92da
Esteban Zepeda <126184363+a01026512@users.noreply.github.com>
2024-03-01 19:36:17 -0600

    Update DataContext.tsx

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -12,21 +12,6 @@ export const DataContext = createContext<IDataContext | null>(null);
 export const DataProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
-    useEffect(() => {
-        // Fetch data from the API
-        const fetchData = async () => {
-            try {
-                const response = await fetch("your-api-url");
-                const data = await response.json();
-                setPokemon(data);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        fetchData(); // Call the fetchData function when the component mounts
-    }, []); // Empty dependency array to run this effect only once when the component mounts
-
     const values: IDataContext = {
         pokemon,
         setPokemon,
